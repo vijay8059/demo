fe9537813129e6d8f1f751c8696b13906bdec49d
Vijay <vijay8059@gmail.com>
2026-01-14 15:16:41 +0530

    Update main.go

--- main.go
+++ main.go
@@ -4,11 +4,21 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Printf("could not get hostname: %v", err)
+		hostname = "unknown"
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, World!")
+		log.Printf("request %s %s from %s served by pod/container %s",
+			r.Method, r.URL.Path, r.RemoteAddr, hostname)
+
+		fmt.Fprintf(w, "Hello, World! from %s\n", hostname)
 	})
 
 	log.Println("Server starting on port 8080...")
